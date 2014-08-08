338325c05db5ab194a40da360f520595621f1d16
John Wallace <john.fritz.wallace@gmail.com>
2014-08-08 13:36:52 -0400

    Added new test file to make sure setup is being cloned. Added geometry package.

---
+++ Java Tower Defense Engine/src/objectGeometry/GitTestFile.java
@@ -0,0 +1,27 @@
+package objectGeometry;
+/**
+ * @author Fritz Wallace
+ *
+ */
+/*
+ * This file serves simply as a test to make sure the java 
+ * enviorment other contributers are working on is functioning.
+ * 
+ */
+public class GitTestFile 
+{
+
+	public void printTestMessage()
+	{
+		System.out.println("Hello World.");
+	}
+	/*
+	 * Prints out the m
+	 */
+	public static void main( String[] args)
+	{
+		GitTestFile testInstance = new GitTestFile();
+		testInstance.printTestMessage();
+		
+	}
+}
---
+++ Java Tower Defense Engine/src/objectGeometry/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author FritzOld
+ *
+ */
+package objectGeometry;
