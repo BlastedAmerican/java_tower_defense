cfe7bb2f7f988ce0a1ccb37e6bdae00020172ba0
John Wallace <john.fritz.wallace@gmail.com>
2014-09-08 15:21:29 -0400

    Added metadata files to attempt to deal with a issue where things where not showing up.

--- Java Tower Defense Engine/src/objectGeometry/ShapeBase.java
+++ Java Tower Defense Engine/src/objectGeometry/ShapeBase.java
@@ -3,8 +3,12 @@ import java.util.*;
 
 public class ShapeBase implements Shape
 {
+	//List of protected points that work with the object.
 	protected ArrayList<Point> PointList;
+	//Key point. Used for various manipulations of Shape location.
 	protected Point keyPoint;
+	//ZLevel. Optional. Hook for displaying objects on various levels.
+	protected int zLevel = 0;
 	/*
 	 * Creates a shape object with no points.
 	 * Shape is not intended to be used as
@@ -16,17 +20,37 @@ public class ShapeBase implements Shape
 	{
 		
 	}
-	@Override
+	/*
+	 * First version. Moves all points that compose the object.
+	 * Takes two values, a X and Y for position.
+	 * @param float xPosition
+	 * @param float yPosition
+	 * @see objectGeometry.Shape#setLocation(float, float)
+	 */
 	public void setLocation(float xPos, float yPos) 
 	{
+		if( keyPoint != null)
+		{
+			float keyPointXLoc = keyPoint.getXPos();
+			float keyPointYLoc = keyPoint.getYPos();
+			float extraShiftY = 0;
+			float extraShiftX = 0;
+			for( Point movingPoint: PointList )
+			{
+				extraShiftX = movingPoint.getXPos() - keyPointXLoc;
+				extraShiftY = movingPoint.getYPos() - keyPointYLoc;
+				movingPoint.setPosition(xPos + extraShiftX,
+										yPos + extraShiftY );
+				
+			}
+		}
 		// TODO Auto-generated method stub
 		
 	}
 	@Override
-	public void setZLevel(int zLevel) 
+	public void setZLevel(int newZLevel) 
 	{
-		// TODO Auto-generated method stub
-		
+		zLevel = newZLevel;
 	}
 	@Override
 	public ArrayList<ArrayList<Point>> getPointPairs() 
@@ -50,6 +74,8 @@ public class ShapeBase implements Shape
 	public Shape copy() 
 	{
 		// TODO Auto-generated method stub
+		// NOTE Must make a copy! Not a reference to pointers in memory!
+		
 		return null;
 	}
 	@Override
@@ -57,6 +83,28 @@ public class ShapeBase implements Shape
 	{
 		// TODO Auto-generated method stub
 		return null;
+	}
+	public String print()
+	{
+		String output = "Empty Shape";
+		
+		if( PointList != null )
+		{
+			output = "Shape - " + PointList.size() + "Points";
+			for( Point n: PointList )
+			{
+				output = output + n.print();
+			}
+		}
+		
+		
+		return output;
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
 	}
 	
 
